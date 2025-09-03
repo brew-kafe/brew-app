29ec9c188f6bff68bb98608e877c26e9591c2b9d
bashlui <luisboltech005@outlook.com>
2025-09-02 20:22:42 -0600

    initial commit for brew application

---
+++ brew/brewApp.swift
@@ -0,0 +1,17 @@
+//
+//  brewApp.swift
+//  brew
+//
+//  Created by toño on 02/09/25.
+//
+
+import SwiftUI
+
+@main
+struct brewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
