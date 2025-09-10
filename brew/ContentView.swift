6b53e01f0fd0958510362a1010659a1cb79605a7
Regina Romero Alvarado <a00840840@tec.mx>
2025-09-10 09:32:55 -0600

    Tab Bar creation

--- brew/ContentView.swift
+++ brew/ContentView.swift
@@ -2,23 +2,75 @@
 //  ContentView.swift
 //  brew
 //
-//  Created by toño on 02/09/25.s
+//  Created by toño on 02/09/25.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("this is tab bar")
+        TabView {
+            NavigationView {
+                HomeView()
+            }
+            .tabItem {
+                Label("Inicio", systemImage: "house.fill")
+            }
+
+            NavigationView {
+                DiagnosticView()
+            }
+            .tabItem {
+                Label("Diagnostico", systemImage: "leaf.fill")
+            }
+
+            NavigationView {
+                MapView()
+            }
+            .tabItem {
+                Label("Mapa", systemImage: "map.fill")
+            }
+            
+            NavigationView {
+                DashboardView()
+            }
+            .tabItem {
+                Label("Tablero", systemImage: "chart.bar.xaxis.ascending")
+            }
         }
-        .padding()
+    }
+}
+
+
+struct HomeView: View {
+    var body: some View {
+        Text("Diagnostico")
+            .navigationTitle("Diagnostico")
+    }
+}
+
+struct DiagnosticView: View {
+    var body: some View {
+        Text("Diagnostico")
+            .navigationTitle("Diagnostico")
+    }
+}
+
+struct MapView: View {
+    var body: some View {
+        Text("Mapa")
+            .navigationTitle("Mapa")
+    }
+}
+
+struct DashboardView: View {
+    var body: some View {
+        Text("Tablero")
+            .navigationTitle("Tablero")
     }
 }
 
 #Preview {
     ContentView()
+    
 }
