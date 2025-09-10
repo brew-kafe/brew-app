43a6328c6a43e1f1439271ac59fadc6f1b827a72
GalileaRestrepo <a01178273@tec.mx>
2025-09-10 07:27:59 -0600

    mvvm setup

--- brew/DataServices/LocationsDataService.swift
+++ brew/DataServices/LocationsDataService.swift
@@ -14,7 +14,7 @@ class LocationsDataService {
         Location(
             name: "Colosseum",
             cityName: "Rome",
-            coordinates: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922),
+            coordinates: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922), //reemplazar con coordinates de un lugar en chiapas
             description: "The Colosseum is an oval amphitheatre in the centre of the city of Rome, Italy, just east of the Roman Forum. It is the largest ancient amphitheatre ever built, and is still the largest standing amphitheatre in the world today, despite its age.",
             imageNames: [
                 "rome-colosseum-1",
--- brew/Views/LocationsView.swift
+++ brew/Views/LocationsView.swift
@@ -9,10 +9,13 @@ import SwiftUI
 
 class LocationsViewModel: ObservableObject{
     
+    
 }
 
 struct LocationsView: View {
+    
     @StateObject private var vm = LocationsViewModel()
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
